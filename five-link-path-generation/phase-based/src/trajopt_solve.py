5793d07489ac6d293b0ed115a69a4d7b5a65ec17
aditya-shirwatkar <cosmosaditya02@gmail.com>
2020-08-15 22:57:08 +0530

    solve formulated

--- five-link-path-generation/phase-based/src/trajopt_solve.py
+++ five-link-path-generation/phase-based/src/trajopt_solve.py
@@ -1,7 +1,15 @@
 import numpy as np
 import casadi as ca
 
-class TrajOptSolve(ca):
+from trajopt_formulation import NLP
+
+class TrajOptSolve():
     def __init__(self):
         super().__init__()
- 
+        self.formulation = NLP(knot_points_per_phase=40, steps=4, total_duration=3, model='biped')
+        p_opts = {"expand":True}
+        s_opts = {"max_iter": 3000}
+        self.formulation.opti.solver("ipopt",p_opts,s_opts)
+    
+problem = TrajOptSolve()
+problem.formulation.opti.solve_limited()
