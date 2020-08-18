aefa13ec186f952f25a4e2b194b799acbcc32d8c
aditya-shirwatkar <cosmosaditya02@gmail.com>
2020-08-19 01:03:48 +0530

    update

--- five-link-path-generation/phase-based/src/trajopt_solve.py
+++ five-link-path-generation/phase-based/src/trajopt_solve.py
@@ -7,7 +7,7 @@ from trajopt_formulation import NLP2
 class TrajOptSolve():
     def __init__(self):
         super().__init__()
-        self.formulation = NLP1(knot_points_per_phase=20, steps=2, total_duration=1, model='biped')
+        self.formulation = NLP1(knot_points_per_phase=40, steps=2, total_duration=1, model='biped')
         p_opts = {"expand":True}
         s_opts = {"max_iter": 3000}
         self.formulation.opti.solver("ipopt",p_opts,s_opts)
