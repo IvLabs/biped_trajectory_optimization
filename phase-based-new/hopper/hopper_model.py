d3f5f4299b438b03335bd6cac731c6897e338591
aditya-shirwatkar <cosmosaditya02@gmail.com>
2020-08-26 18:10:41 +0530

    made centeroidal dynamics function

--- phase-based-new/hopper/hopper_model.py
+++ phase-based-new/hopper/hopper_model.py
@@ -5,100 +5,72 @@ import helper_functions as hf
 class Hopper():
     def __init__(self):
         super().__init__()
-        self.num_ee    = 1
-        self.length    = np.array([0.5,0.5,0.5])
-        self.mass      = np.array([0.1,0.1,0.5])
-        self.initial_q = np.zeros((3,1))
-        self.final_q   = np.zeros((3,1))
-        self.inertia   = self.mass * (self.length**2) /12
-        self.gravity   = -10
+        self.num_ee  = 1
+        self.length  = np.array([0.5,0.5,0.5])
+        self.mass    = np.array([0.1,0.1,0.5])
+        self.i_qcom  = np.zeros((1,1))
+        self.f_qcom  = np.zeros((1,1))
+        self.inertia = self.mass * (self.length**2) /12
+        self.gravity = 10
+
+        self.mcom = np.sum(self.mass).reshape(1,1)
+        self.icom = np.max(self.inertia).reshape(1,1)
 
         self.gravity_vector = ca.MX.zeros(2)
         self.gravity_vector[1] = self.gravity
         
-        self.b = ca.mmax(self.length*2)
+        self.b = ca.mmax(self.length*3)
         self.a = ca.mmax(self.length)
 
-    def setFullState(self, q, dq, c3, dc3, u, f10):
-        self.q   = ca.reshape( q, 3, 1)
-        self.dq  = ca.reshape(dq, 3, 1)
+    def setState(self, r, r_dot, q, q_dot, pe, f):
+        self.q     = ca.reshape(q    , 1, 1)
+        self.q_dot = ca.reshape(q_dot, 1, 1)
+        self.r     = ca.reshape(r    , 2, 1)
+        self.r_dot = ca.reshape(r_dot , 2, 1)
+        self.pe    = ca.reshape(pe   , 2, 1)
+        self.f     = ca.reshape(f    , 2, 1)
 
-        self.c3   = ca.reshape(  c3, 2, 1)
-        self.dc3  = ca.reshape( dc3, 2, 1)
+        self.r_ddot, self.q_ddot = self.getCenteroidalDynamics()
 
-        self.u   = ca.reshape(  u, 2, 1)
-        self.f10 = ca.reshape(f10, 2, 1)
+        self.centeroidal_dynamics = ca.Function('CD', [r, r_dot, q, q_dot, pe, f], 
+                                                      [self.r_ddot, self.q_ddot], 
+                                                      ['r','r_dot','q','q_dot','pe','f'],
+                                                      ['r_ddot','q_ddot'])
 
-        self.p, self.dp, self.ddp, self.c, self.dc, self.ddc = self.getKinematics()
-        self.dynamics = self.getDynamics()
+    def getCenteroidalDynamics(self):
+        q     = self.q    
+        q_dot = self.q_dot
+        r     = self.r    
+        r_dot = self.r_dot 
+        pe    = self.pe   
+        f     = self.f   
 
-    def getKinematics(self):        
-        # Assume p,c as 2x1
+        mcom  = self.mcom
+        g     = self.gravity_vector
+        icom  = self.icom
 
-        ##################
-        #####--Leg--######
-        ##################
-        p = ca.MX.zeros(2, 4)
-        c = ca.MX.zeros(2, 3)
+        r_ddot = (f - mcom*g)/mcom
+        q_ddot = hf.crossProduct2D(f, r - pe)/icom 
 
-        c3 = self.c3
-
-        p[0,3],p[1,3] =  self.length[2]*ca.sin(self.q[2])/2 + c3[0,0],  self.length[2]*ca.cos(self.q[2])/2 + c3[1,0]
-        p[0,2],p[1,2] = -self.length[2]*ca.sin(self.q[2])/2 + c3[0,0], -self.length[2]*ca.cos(self.q[2])/2 + c3[1,0]
-        p[0,1],p[1,1] = -self.length[1]*ca.sin(self.q[1]) + p[0,2]   , -self.length[1]*ca.cos(self.q[1]) + p[1,2]
-        p[0,0],p[1,0] = -self.length[0]*ca.sin(self.q[0]) + p[0,1]   , -self.length[1]*ca.cos(self.q[0]) + p[1,1]
-
-        c[0,2],c[1,2] = c3[0,0], c3[1,0]
-        c[0,1],c[1,1] = -self.length[1]*ca.sin(self.q[1])/2 + p[0,2], -self.length[1]*ca.cos(self.q[1])/2 + p[1,2]
-        c[0,0],c[1,0] = -self.length[0]*ca.sin(self.q[0])/2 + p[0,1], -self.length[0]*ca.cos(self.q[0])/2 + p[1,1]
- 
-        ###--Derivatives--###
-        dp  = ca.jtimes( p, self.q, self.dq) + self.dc3
-        dc  = ca.jtimes( c, self.q, self.dq) + self.dc3
-
-        ddp = ca.jtimes(dp, self.q, self.dq**2)
-        ddc = ca.jtimes(dc, self.q, self.dq**2)
-
-
-        ###--Form a dictionary--###
-        p   = {'Leg' :   p, 'Constraint' : (self.q[0]<=self.q[1])}
-        dp  = {'Leg' :  dp}
-        ddp = {'Leg' : ddp}
-
-        c   = {'Leg' :   c}
-        dc  = {'Leg' :  dc}
-        ddc = {'Leg' : ddc}
-
-        return p,dp,ddp,c,dc,ddc      
-
-    def getDynamics(self):
-
-        ###--Leg--###
-        ddq  = ca.MX.zeros(3)
-        c3  = self.c3
-        p   = self.p  ['Leg']
-        c   = self.c  ['Leg']
-        ddc = self.ddc['Leg']
-        f10 = self.f10
-
-        f32 = self.mass[2]*(ddc[:,2] - self.gravity_vector)
-        ddq[2] = self.u[1] + hf.crossProduct2D(p[:,2]-c[:,2], f32)
-
-        f12 = self.mass[0]*(ddc[:,0] - self.gravity_vector) - f10
-        ddq[0] = -self.u[0] + hf.crossProduct2D(p[:,1]-c[:,0], f12) + hf.crossProduct2D(p[:,0]-c[:,0], f10)
-
-        f21 = -f12
-        f23 = -f32
-        ddq[1] = self.u[0]-self.u[1] + hf.crossProduct2D(p[:,2]-c[:,1], f23) + hf.crossProduct2D(p[:,1]-c[:,1], f21)
-
-        ddq /= self.inertia
-
-        dynamics = {'Leg': ddq}
-
-        return dynamics
+        return r_ddot, q_ddot
 
 # test check for sanity
 
+# test_hopper = Hopper()
+# q     = ca.MX.sym('q    ', 1, 1)
+# q_dot = ca.MX.sym('q_dot', 1, 1)
+# r     = ca.MX.sym('r    ', 2, 1)
+# r_dot = ca.MX.sym('r_dot', 2, 1)
+# pe    = ca.MX.sym('pe   ', 2, 1)
+# f     = ca.MX.sym('f    ', 2, 1)
+
+# test_hopper.setState(r, r_dot, q, q_dot, pe, f)
+
+# print(test_hopper.centeroidal_dynamics(r, r_dot, q, q_dot, pe, f))
+
+###############################################
+###############################################
+###############################################
 
 # test_hopper = Hopper()
 # q    = ca.MX.sym(   'q', 3, 1)
@@ -114,3 +86,85 @@ class Hopper():
 # test_hopper.setFullState(q, dq, c3, dc3, ddc3, u, f10)
 
 # print(test_hopper.dynamics)
+
+###############################################
+###############################################
+###############################################
+
+    # def setFullState(self, q, dq, c3, dc3, u, f10):
+    #     self.q   = ca.reshape( q, 3, 1)
+    #     self.dq  = ca.reshape(dq, 3, 1)
+
+    #     self.c3   = ca.reshape(  c3, 2, 1)
+    #     self.dc3  = ca.reshape( dc3, 2, 1)
+
+    #     self.u   = ca.reshape(  u, 2, 1)
+    #     self.f10 = ca.reshape(f10, 2, 1)
+
+    #     self.p, self.dp, self.ddp, self.c, self.dc, self.ddc = self.getKinematics()
+    #     self.dynamics = self.getDynamics()
+
+    # def getKinematics(self):        
+    #     # Assume p,c as 2x1
+
+    #     ##################
+    #     #####--Leg--######
+    #     ##################
+    #     p = ca.MX.zeros(2, 4)
+    #     c = ca.MX.zeros(2, 3)
+
+    #     c3 = self.c3
+
+    #     p[0,3],p[1,3] =  self.length[2]*ca.sin(self.q[2])/2 + c3[0,0],  self.length[2]*ca.cos(self.q[2])/2 + c3[1,0]
+    #     p[0,2],p[1,2] = -self.length[2]*ca.sin(self.q[2])/2 + c3[0,0], -self.length[2]*ca.cos(self.q[2])/2 + c3[1,0]
+    #     p[0,1],p[1,1] = -self.length[1]*ca.sin(self.q[1]) + p[0,2]   , -self.length[1]*ca.cos(self.q[1]) + p[1,2]
+    #     p[0,0],p[1,0] = -self.length[0]*ca.sin(self.q[0]) + p[0,1]   , -self.length[1]*ca.cos(self.q[0]) + p[1,1]
+
+    #     c[0,2],c[1,2] = c3[0,0], c3[1,0]
+    #     c[0,1],c[1,1] = -self.length[1]*ca.sin(self.q[1])/2 + p[0,2], -self.length[1]*ca.cos(self.q[1])/2 + p[1,2]
+    #     c[0,0],c[1,0] = -self.length[0]*ca.sin(self.q[0])/2 + p[0,1], -self.length[0]*ca.cos(self.q[0])/2 + p[1,1]
+ 
+    #     ###--Derivatives--###
+    #     dp  = ca.jtimes( p, self.q, self.dq) + self.dc3
+    #     dc  = ca.jtimes( c, self.q, self.dq) + self.dc3
+
+    #     ddp = ca.jtimes(dp, self.q, self.dq**2)
+    #     ddc = ca.jtimes(dc, self.q, self.dq**2)
+
+
+    #     ###--Form a dictionary--###
+    #     p   = {'Leg' :   p, 'Constraint' : (self.q[0]<=self.q[1])}
+    #     dp  = {'Leg' :  dp}
+    #     ddp = {'Leg' : ddp}
+
+    #     c   = {'Leg' :   c}
+    #     dc  = {'Leg' :  dc}
+    #     ddc = {'Leg' : ddc}
+
+    #     return p,dp,ddp,c,dc,ddc      
+
+    # def getDynamics(self):
+
+    #     ###--Leg--###
+    #     ddq  = ca.MX.zeros(3)
+    #     c3  = self.c3
+    #     p   = self.p  ['Leg']
+    #     c   = self.c  ['Leg']
+    #     ddc = self.ddc['Leg']
+    #     f10 = self.f10
+
+    #     f32 = self.mass[2]*(ddc[:,2] - self.gravity_vector)
+    #     ddq[2] = self.u[1] + hf.crossProduct2D(p[:,2]-c[:,2], f32)
+
+    #     f12 = self.mass[0]*(ddc[:,0] - self.gravity_vector) - f10
+    #     ddq[0] = -self.u[0] + hf.crossProduct2D(p[:,1]-c[:,0], f12) + hf.crossProduct2D(p[:,0]-c[:,0], f10)
+
+    #     f21 = -f12
+    #     f23 = -f32
+    #     ddq[1] = self.u[0]-self.u[1] + hf.crossProduct2D(p[:,2]-c[:,1], f23) + hf.crossProduct2D(p[:,1]-c[:,1], f21)
+
+    #     ddq /= self.inertia
+
+    #     dynamics = {'Leg': ddq}
+
+    #     return dynamics
